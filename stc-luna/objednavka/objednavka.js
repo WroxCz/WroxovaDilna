f01ae28308e1a7125f32feb80698c0414821c472
WroxCz <linyvlk@gmail.com>
2026-05-25 21:51:01 +0200

    Add files via upload

--- stc-luna/objednavka/objednavka.js
+++ stc-luna/objednavka/objednavka.js
@@ -6,3 +6,11 @@ document
 
     alert("Objednávka odeslána do Forge World.");
 });
+const params = new URLSearchParams(window.location.search);
+
+const modelName = params.get("model");
+
+if(modelName){
+
+    document.querySelector("#modelInput").value = modelName;
+}
