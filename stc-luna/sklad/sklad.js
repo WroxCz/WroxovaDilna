480af7b8758e6cf4b7f315f57fbb04205dc139ad
WroxCz <linyvlk@gmail.com>
2026-05-28 23:09:14 +0200

    Add files via upload

--- stc-luna/sklad/sklad.js
+++ stc-luna/sklad/sklad.js
@@ -2,21 +2,30 @@ let allMaterials = [];
 
 let activeMaterial = "Vše";
 let activeManufacturer = "Vše";
-let activeVariant = "Vše";
+let activeVariant = "";
+
+const manufacturerBar =
+document.getElementById(
+    "manufacturer-bar"
+);
+
+const variantBar =
+document.getElementById(
+    "variant-bar"
+);
 
 Promise.all([
 
-    fetch("data/pla-elegoo.json")
-        .then(response => response.json()),
 
     fetch("data/pla-alzamet.json")
         .then(response => response.json()),
     
-    fetch("data/pla-alzamet-mat.json")
-        .then(response => response.json()),        
-
     fetch("data/pla-temu.json")
-        .then(response => response.json())          
+        .then(response => response.json()),
+     
+    fetch("data/ProfLab.json")
+    .then(response => response.json())    
+
 ])
 
 .then(allData => {
@@ -24,6 +33,8 @@ Promise.all([
     allMaterials = allData.flat();
 
     renderMaterials(allMaterials);
+
+    updateVisibleFilters();
 
 });
 
@@ -79,30 +90,200 @@ function renderMaterials(data){
 
 function applyFilters(){
 
+    activeManufacturer = activeManufacturer.trim();
+activeVariant = activeVariant.trim();
+activeMaterial = activeMaterial.trim();
+
+ console.log({
+        material: activeMaterial,
+        manufacturer: activeManufacturer,
+        variant: activeVariant
+    })
+
     let filtered = allMaterials;
 
     if(activeMaterial !== "Vše"){
 
         filtered = filtered.filter(material =>
-            material.material === activeMaterial
+            material.material.toLowerCase() === activeMaterial.toLowerCase()
         );
     }
 
     if(activeManufacturer !== "Vše"){
 
-        filtered = filtered.filter(material =>
-            material.manufacturer === activeManufacturer
-        );
-    }
+    filtered = filtered.filter(material =>
+        material.manufacturer.trim() === activeManufacturer.trim()
+    );
+}
 
-    if(activeVariant !== "Vše"){
+    if(activeVariant !== ""){
 
-        filtered = filtered.filter(material =>
-            material.variant === activeVariant
-        );
-    }
+    filtered = filtered.filter(material =>
+        material.variant.trim() === activeVariant
+    );
+}
 
     renderMaterials(filtered);
+}
+function updateVisibleFilters(){
+
+    /* KDYŽ JE VŠE */
+
+    if(activeMaterial === "Vše"){
+
+    activeManufacturer = "Vše";
+    activeVariant = "Vše";
+
+    manufacturerBar.style.display =
+    "none";
+
+    variantBar.style.display =
+    "none";
+
+    /* OBNOVIT BUTTONY */
+
+    document
+    .querySelectorAll(
+        ".manufacturer-button"
+    )
+
+    .forEach(button => {
+
+        button.style.display =
+        "inline-block";
+    });
+
+    document
+    .querySelectorAll(
+        ".variant-button"
+    )
+
+    .forEach(button => {
+
+        button.style.display =
+        "inline-block";
+    });
+
+    return;
+}
+
+    manufacturerBar.style.display =
+"flex";
+
+if(activeManufacturer === "Vše"){
+
+    variantBar.style.display =
+    "none";
+}
+
+else{
+
+    variantBar.style.display =
+    "flex";
+}
+
+    /* VÝROBCI */
+
+    const availableManufacturers =
+    allMaterials
+
+    .filter(material =>
+        material.material.toLowerCase() === activeMaterial.toLowerCase()
+    )
+
+    .map(material =>
+        material.manufacturer
+    );
+
+    if(
+    activeManufacturer !== "Vše"
+    &&
+    !availableManufacturers.includes(activeManufacturer)
+){
+    activeManufacturer = "Vše";
+}
+
+    document
+    .querySelectorAll(
+        ".manufacturer-button"
+    )
+
+    .forEach(button => {
+
+        if(
+            button.innerText.trim() === "Vše"
+            ||
+            availableManufacturers.includes(
+                button.innerText.trim()
+            )
+        ){
+
+            button.style.display =
+            "inline-block";
+        }
+
+        else{
+
+            button.style.display = "none";
+            button.classList.remove("active");
+        }
+    });
+
+    /* VARIANTY */
+
+    const availableVariants =
+allMaterials
+
+.filter(material =>
+
+    material.material.toLowerCase() === activeMaterial.toLowerCase()
+
+    &&
+
+    (
+        activeManufacturer === "Vše"
+        ||
+        material.manufacturer.toLowerCase() === activeManufacturer.toLowerCase()
+    )
+)
+
+.map(material =>
+    material.variant
+);
+
+if(
+    activeManufacturer !== "Vše"
+    &&
+    !availableManufacturers.includes(activeManufacturer)
+){
+    activeManufacturer = "Vše";
+}
+
+    document
+    .querySelectorAll(
+        ".variant-button"
+    )
+
+    .forEach(button => {
+
+        if(
+            button.innerText.trim() === "Vše"
+            ||
+            availableVariants.includes(
+                button.innerText.trim()
+            )
+        ){
+
+            button.style.display =
+            "inline-block";
+        }
+
+        else{
+
+            button.style.display =
+            "none";
+        }
+    });
 }
 
 /* MATERIÁL */
@@ -124,9 +305,41 @@ document
         button.classList.add("active");
 
         activeMaterial =
-        button.innerText;
+        button.innerText.trim();
+
+        activeManufacturer = "Vše";
+        activeVariant = "";
+
+        /* RESET ACTIVE BUTTONŮ */
+
+        document
+        .querySelectorAll(".manufacturer-button")
+        .forEach(btn =>
+            btn.classList.remove("active")
+        );
+
+        document
+        .querySelectorAll(".variant-button")
+        .forEach(btn =>
+            btn.classList.remove("active")
+        );
+
+        /* AKTIVOVAT VŠE */
+
+        document
+        .querySelectorAll(".variant-button")
+        .forEach((btn) => {
+
+            if(btn.innerText.trim() === "Vše"){
+
+                btn.classList.add("active");
+            }
+        });
+
+        updateVisibleFilters();
 
         applyFilters();
+
     });
 });
 
@@ -149,7 +362,9 @@ document
         button.classList.add("active");
 
         activeManufacturer =
-        button.innerText;
+        button.innerText.trim();
+
+        updateVisibleFilters();
 
         applyFilters();
     });
@@ -174,7 +389,7 @@ document
         button.classList.add("active");
 
         activeVariant =
-        button.innerText;
+        button.innerText.trim();
 
         applyFilters();
     });
