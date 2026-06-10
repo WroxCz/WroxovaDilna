0debee1a57f01d62c795a68826b5a33cbe288c41
WroxCz <linyvlk@gmail.com>
2026-06-10 07:59:28 +0200

    oprava01

--- stc-luna/modely/testovaci-modely/Tolerance/tolerance.js
+++ stc-luna/modely/testovaci-modely/Tolerance/tolerance.js
@@ -1,14 +1,14 @@
 export const modely = [
     {
         name: "Clearance Tolerance Test",
-        path: "./testovaci-modely/Tolerance/Clearance Tolerance Test/model.html"
+        path: "./testovaci-modely/Tolerance/clearance-tolerance-test/model.html"
     },
     {
         name: "Print in Place Tolerance Test",
-        path: "./testovaci-modely/Tolerance/Print in Place Tolerance Test/model.html"
+        path: "./testovaci-modely/Tolerance/print-in-place-tolerance-test/model.html"
     },
     {
         name: "Tolerance gauge Fidget toy",
-        path: "./testovaci-modely/Tolerance/Tolerance gauge Fidget toy/model.html"
+        path: "./testovaci-modely/Tolerance/tolerance-gauge-fidget-toy/model.html"
     }
 ];
