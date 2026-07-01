51c808c70022166f774047ded38423cd5fe30bff
WroxCz <linyvlk@gmail.com>
2026-07-01 07:20:41 +0200

    armiger

---
+++ stc-luna/modely/projekty/w40k/imperialni-rytiri/armiger/armiger.js
@@ -0,0 +1,6 @@
+export const modely = [
+{
+    name: "Martian Walker Platform",
+    path: "./projekty/w40k/imperialni-rytiri/armiger/martian_walker_platform/index.html"
+}
+];
--- stc-luna/modely/projekty/w40k/imperialni-rytiri/imperialni-rytiri.js
+++ stc-luna/modely/projekty/w40k/imperialni-rytiri/imperialni-rytiri.js
@@ -2,5 +2,9 @@ export const sekce = [
 {
     name: "Questoris",
     file: "./projekty/w40k/imperialni-rytiri/questoris/questoris.js"
+},
+{
+    name: "Armiger",
+    file: "./projekty/w40k/imperialni-rytiri/armiger/armiger.js"
 }
 ];
