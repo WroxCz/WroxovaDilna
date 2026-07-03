5ad0f4c1025241f9eec9ded649de149d050b49ab
WroxCz <linyvlk@gmail.com>
2026-07-03 16:08:55 +0200

    nefunguje

--- stc-luna/modely/komercni/dekorace/lode/lode.js
+++ stc-luna/modely/komercni/dekorace/lode/lode.js
@@ -1,6 +1,6 @@
 export const modely = [
     {
         name: "Catamar Benchy",
-        //path: "./komercni/dekorace/lode/Catamar-Benchy-floats/model.html"
+        //path: "./komercni/dekorace/lode/Catamar-Benchy-floats/model.html" nefunguje
     }
 ];
